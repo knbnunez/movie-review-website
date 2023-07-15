499efeb99d69d97e12583666954cf94e1edc1fc1
knbnunez <106483091+knbnunez@users.noreply.github.com>
2023-07-15 19:40:21 -0300

    Solución error de un import invisible?

--- App_peliculas/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .actores import ActoresTV, ActorDetalleTV
-from .directores import DirectoresTV, DirectorDetalleTV
-from .home import HomeTV
-from .peliculas import PeliculasTV, PeliculaDetalleTV, PeliculasBuscadasTV
